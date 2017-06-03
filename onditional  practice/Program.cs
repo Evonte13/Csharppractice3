82eabdab745f6bd04b0ecf7e56c1aaad056c7de4
evonte dunn <evontedunn@gmail.com>
2017-06-02 20:23:55 -0400

    first commit

---
+++ onditional  practice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace onditional__practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            int userAge;
+
+            Console.WriteLine("What Is your age");
+
+            userAge = int.Parse(Console.ReadLine());
+
+
+            if(userAge >= 17)
+            {
+                Console.WriteLine(userAge + "  can go see movie ");
+
+            }
+            else
+            {
+                Console.WriteLine(userAge + " you cannot see movie ");
+            }
+
+
+
+
+            
+            
+
+        }
+    }
+}
